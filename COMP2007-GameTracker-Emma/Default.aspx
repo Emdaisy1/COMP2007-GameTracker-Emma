1afbafd30bfba3c7e88c9691695319e0a70fd5cd
Emdaisy1 <hilborn_emmav@live.ca>
2016-06-15 22:31:41 -0400

    Calculate and display data to page

--- COMP2007-GameTracker-Emma/Default.aspx
+++ COMP2007-GameTracker-Emma/Default.aspx
@@ -13,14 +13,14 @@ Description: Main page which will display game stats AND team stats to user - lo
     <asp:GridView runat="server" ID="GamesGridView" AutoGenerateColumns="false"
         CssClass="table table-striped table-bordered table-hover">
         <Columns>
+            <asp:BoundField DataField="GameNum" HeaderText="Game" Visible="true" />
             <asp:BoundField DataField="Week" HeaderText="Week" Visible="true" />
             <asp:BoundField DataField="Description" HeaderText="Description" Visible="true" />
-            <asp:BoundField DataField="TeamOneID" HeaderText="Team One" Visible="true" />
-            <asp:BoundField DataField="TeamOnePoints" HeaderText="Team One Pts" Visible="true" />
-            <asp:BoundField DataField="TeamTwoID" HeaderText="Team Two" Visible="true" />
-            <asp:BoundField DataField="TeamTwoID" HeaderText="Team Two Points" Visible="true" />
+            <asp:BoundField DataField="TeamOne" HeaderText="Team One" Visible="true" />
+            <asp:BoundField DataField="TeamTwo" HeaderText="Team Two" Visible="true" />
+            <asp:BoundField DataField="TotalPoints" HeaderText="Total Points" Visible="true" />
             <asp:BoundField DataField="Spectators" HeaderText="Spectators" Visible="true" />
-            <asp:BoundField DataField="Winner" HeaderText="Winner" Visible="true" />
+            <asp:BoundField DataField="GameWinner" HeaderText="Winner" Visible="true" />
         </Columns>
     </asp:GridView>
 
--- COMP2007-GameTracker-Emma/Default.aspx.cs
+++ COMP2007-GameTracker-Emma/Default.aspx.cs
@@ -4,40 +4,128 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
 using COMP2007_GameTracker_Emma.Models;
 using System.Web.ModelBinding;
+using System.Data.SqlClient;
 
 namespace COMP2007_GameTracker_Emma
 {
     public partial class Default : System.Web.UI.Page
     {
+        SqlConnection db = new SqlConnection("user id = EmmaH; data source = comp2007emma.database.windows.net; initial catalog = comp2007emma; persist security info = True; user id = EmmaH; password = Emma1031");
+        SqlDataReader reader;
+        DataTable gamesData = new DataTable();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 this.GetGames();
+                this.GetTeams();
             }
         }
 
         protected void GetGames()
         {
-            using (DefaultConnection db = new DefaultConnection())
+            
+            int weekNum = 24;
+
+            SqlCommand getGames = new SqlCommand("SELECT *, (TeamOnePoints+TeamTwoPoints) AS TotalPoints FROM GamesTable WHERE Week = @Week", db);
+            getGames.Parameters.AddWithValue("@Week", weekNum);
+            db.Open();
+            reader = getGames.ExecuteReader();
+            gamesData.Load(reader);
+            db.Close();
+
+            gamesData.Columns.Add(new DataColumn("TeamOne", typeof(string)));
+            gamesData.Columns.Add(new DataColumn("TeamTwo", typeof(string)));
+            gamesData.Columns.Add(new DataColumn("GameWinner", typeof(string)));
+            gamesData.Columns.Add(new DataColumn("GameNum", typeof(string)));
+
+            int gameNum = 1;
+
+            foreach (DataRow row in gamesData.Rows)
             {
-                int weekNum = 24;
-                var Games = (from allGames in db.GamesTables
-                             where allGames.Week == weekNum
-                                select allGames);
+                row["GameNum"] = "Game #" + gameNum;
+                gameNum++;
 
-                GamesGridView.DataSource = Games.AsQueryable().ToList();
-                GamesGridView.DataBind();
+                if (Int32.Parse(row["TeamOneID"].ToString()) == 0)
+                {
+                    row["TeamOne"] = "";
+                }
+                else
+                {
+                    SqlCommand getTeamNames = new SqlCommand("SELECT TeamName FROM TeamTable WHERE TeamID = @ID", db);
+                    getTeamNames.Parameters.AddWithValue("@ID", row["TeamOneID"].ToString());
+                    db.Open();
+                    reader = getTeamNames.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        row["TeamOne"] = (String)reader["TeamName"];
+                    }
+                    db.Close();
+                }
 
-                var Teams = (from allTeams in db.TeamTables
-                             select allTeams);
-                TeamsGridView.DataSource = Teams.AsQueryable().ToList();
-                TeamsGridView.DataBind();
+                if (Int32.Parse(row["TeamTwoID"].ToString()) == 0)
+                {
+                    row["TeamTwo"] = "";
+                }
+                else
+                {
+                    SqlCommand getTeamNames = new SqlCommand("SELECT TeamName FROM TeamTable WHERE TeamID = @ID", db);
+                    getTeamNames.Parameters.AddWithValue("@ID", row["TeamTwoID"].ToString());
+                    db.Open();
+                    reader = getTeamNames.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        row["TeamTwo"] = (String)reader["TeamName"];
+                    }
+                    db.Close();
+                }
+
+                if (Int32.Parse(row["Winner"].ToString()) == 0)
+                {
+                    row["GameWinner"] = "Tie";
+                }
+                else
+                {
+                    SqlCommand getWinner = new SqlCommand("SELECT TeamName FROM TeamTable WHERE TeamID = @winID", db);
+                    getWinner.Parameters.AddWithValue("@winID", row["Winner"].ToString());
+                    db.Open();
+                    reader = getWinner.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        row["GameWinner"] = (String)reader["TeamName"];
+                    }
+                    db.Close();
+                }
             }
 
+
+            GamesGridView.DataSource = gamesData.DefaultView;
+            GamesGridView.DataBind();
+
+            /*
+            var Games = (from allGames in db.GamesTables
+                         where allGames.Week == weekNum
+                         select allGames);
+
+            GamesGridView.DataSource = Games.AsQueryable().ToList();
+            GamesGridView.DataBind();
+            */
+
+
+        }
+
+        protected void GetTeams()
+        { 
+            /*
+             * var Teams = (from allTeams in db.TeamTables
+                         select allTeams);
+            TeamsGridView.DataSource = Teams.AsQueryable().ToList();
+            TeamsGridView.DataBind();
+            */
         }
     }
 }
